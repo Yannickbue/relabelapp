a431a46f1d196ace7f7edf175dc4737aa2505d08
Yannickbue <yannick.buelens97@gmail.com>
2024-10-09 13:00:54 +0200

    Update cd2.js

--- cd2.js
+++ cd2.js
@@ -1696,7 +1696,7 @@ function create_if_block$7(ctx) {
       if (!mounted) {
         dispose = [
           listen(button0, "click", prevent_default(ctx[2])),
-          listen(button1, "click", prevent_default(ctx[3]))
+          listen(button1, "click", prevent_default(ctx[3])),
           listen(button2, "click", prevent_default(fetchInsights))
         ];
         mounted = true;
